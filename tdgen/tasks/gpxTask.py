66b1283b3bf3ed2adcc81d2fed1c8b4b90a2088c
Janna Hopp <795581+bytehexe@users.noreply.github.com>
2025-10-18 19:26:49 +0200

    Refactor day GPX creation

--- tdgen/tasks/gpxTask.py
+++ tdgen/tasks/gpxTask.py
@@ -3,14 +3,12 @@ import gpxpy
 import gpxpy.gpx
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
-import hdbscan
-import numpy as np
 from tdgen.geoCluster import GeoCluster
 from doit import create_after
 from tabulate import tabulate
-import warnings
 import sys
 import bisect
+from tdgen.gpxCreator import GpxCreator
 
 class GPXTask(BaseTask):
     def __init__(self):
@@ -53,76 +51,11 @@ class GPXTask(BaseTask):
         return filename
 
     def __gpx2gpx(self, date, dst):
-        gpx_out = gpxpy.gpx.GPX()
-
-        coords = []
-
-        for source in self.__sources:    
-            with open(source, "r", encoding="utf-8") as f:
-                gpx = gpxpy.parse(f)
-            for mwpt in gpx.waypoints:
-                if mwpt.time is not None and mwpt.time.date() == date:
-                    gpx_out.waypoints.append(mwpt)
-            for trk in gpx.tracks:
-                new_trk = gpxpy.gpx.GPXTrack(name=trk.name, description=trk.description)
-                for seg in trk.segments:
-                    new_seg = gpxpy.gpx.GPXTrackSegment()
-                    for pt in seg.points:
-                        if pt.time is not None and pt.time.date() == date:
-                            new_seg.points.append(pt)
-                            coords.append([pt.latitude, pt.longitude])
-                    if len(new_seg.points) > 0:
-                        new_trk.segments.append(new_seg)
-                if len(new_trk.segments) > 0:
-                    gpx_out.tracks.append(new_trk)
-            for rte in gpx.routes:
-                new_rte = gpxpy.gpx.GPXRoute(name=rte.name, description=rte.description)
-                for pt in rte.points:
-                    if pt.time is not None and pt.time.date() == date:
-                        new_rte.points.append(pt)
-                if len(new_rte.points) > 0:
-                    gpx_out.routes.append(new_rte)
-
-        if len(coords) > 10:
-            coords = np.array(coords)
-
-            # Fit HDBSCAN
-            clusterer = hdbscan.HDBSCAN(min_cluster_size=1000, metric='haversine')
-            with warnings.catch_warnings():
-                warnings.simplefilter(action='ignore', category=FutureWarning)
-                clusterer.fit(np.radians(coords))
-
-            labels = clusterer.labels_
-            for label in set(labels):
-                if label == -1:
-                    continue
-                cluster_coords = coords[labels == label]
-                cluster = GeoCluster(cluster_coords)
-
-                if cluster.radius > 10000:
-                    # Ignore overly large clusters
-                    continue
-
-                mlat, mlon = cluster.mass_point
-                mwpt = gpxpy.gpx.GPXWaypoint(
-                    latitude=mlat, longitude=mlon,
-                    name=f"Cluster {label}",
-                    description=f"Cluster of {len(cluster_coords)} points and radius {cluster.radius:.1f} m",
-                    symbol="cluster-mass"
-                )
-                gpx_out.waypoints.append(mwpt)
-                clat, clon = cluster.midpoint
-                cwpt = gpxpy.gpx.GPXWaypoint(                
-                    latitude=clat, longitude=clon,
-                    name=f"Cluster {label} Center",
-                    description=f"Center of cluster {label}",
-                    symbol="cluster-center",
-                    position_dilution=cluster.radius
-                )
-                gpx_out.waypoints.append(cwpt)
+        gc = GpxCreator(date, self.__sources)
+        gpx_out = gc.to_xml()
 
         with open(dst, "w", encoding="utf-8") as f:
-            f.write(gpx_out.to_xml())
+            f.write(gpx_out)
 
     @create_after("geo2gpx", target_regex=r'.*\.gpx')
     def task_gpx2gpx(self):
@@ -221,7 +154,7 @@ class GPXTask(BaseTask):
         return {
             "actions": [_update_positions],
             "file_dep": [str(src) for src in self.__sources],
-            "uptodate": False,
+            "uptodate": [False],
             "verbosity": 2,
         }
         
