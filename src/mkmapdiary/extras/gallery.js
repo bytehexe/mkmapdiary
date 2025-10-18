77719f49a541993580c942d1e489558f823a6bba
Janna Hopp <795581+bytehexe@users.noreply.github.com>
2025-10-18 19:35:04 +0200

    Update gallery format

---
+++ src/mkmapdiary/extras/gallery.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    // Justified Gallery
+
+    function rem2px(rem) {    
+        return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+    }
+
+    var base_height_rem = 6;
+    var aspectRatios = Array.from(document.getElementById("photo_gallery").querySelectorAll("img")).map((x) => (Math.max(1, x.offsetHeight / x.offsetWidth)))
+    var avgAspectRatio = aspectRatios.reduce((a, b) => a + b, 0) / aspectRatios.length;
+    var rowHeight = Math.min(avgAspectRatio * rem2px(base_height_rem), rem2px(2*base_height_rem));
+
+    console.log("avgAspectRatio", avgAspectRatio, "rowHeight", rowHeight);
+
+    $("#photo_gallery p").justifiedGallery({
+        rowHeight: rowHeight,
+        margins: 3,
+        lastRow: 'center',
+    });
+
+    window.addEventListener('resize', () => {
+        var rowHeight = Math.min(avgAspectRatio * rem2px(base_height_rem), rem2px(2*base_height_rem));
+        $("#photo_gallery p").justifiedGallery('option', {rowHeight: rowHeight});
+    });
+
+});
